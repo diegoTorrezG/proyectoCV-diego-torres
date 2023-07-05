f2332890687bbbe9a3e89653fe2de0dc382728df
Diego Torres <torrezdife21@gmail.com>
2023-07-04 22:19:52 -0500

    integracion fornt-back

---
+++ script.js
@@ -0,0 +1,39 @@
+console.log("load JS final aqui es........")
+
+function saveForm(){
+
+    let nameForm = document.getElementById("name");
+    let lastNameForm = document.getElementById("lastName");
+    let countryForm = document.getElementById("country");
+    let emailForm = document.getElementById("email");
+
+
+    let form = {
+        name: nameForm.value,
+        lastName: lastNameForm.value,
+        email: emailForm.value,
+        country: countryForm.value
+    }
+
+    console.log(form);
+
+    let url = "http://localhost:8000/api/form";
+    let params = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json" 
+        },
+        body: JSON.stringify(form),
+    }
+
+    fetch(url, params).then(response =>{
+        console.log(response);
+
+        if(response.status == 200){
+            alert("Formulario enviado");
+        }else{
+            alert("Error en el envio del formuario");
+        }
+    });
+    return true;
+}
